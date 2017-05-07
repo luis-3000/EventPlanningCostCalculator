293043f7556a8e516c3a45a73cc15fb0ce15ef7b
Jose Castillo <Jose Castillo>
2017-05-06 23:16:03 -0500

    Set up the initial variables I am going to need and the corresponding constructor. Will work out the details and calculations in the following steps.

--- EventPlanningCostCalculator/BirthdayParty.cs
+++ EventPlanningCostCalculator/BirthdayParty.cs
@@ -8,6 +8,21 @@ namespace EventPlanningCostCalculator
 {
     class BirthdayParty
     {
+        public const int CostOfFoodPerPerson = 25;
 
+        public int NumberOfPeople { get; set; }
+
+        public bool FancyDecorations { get; set; }
+
+        public string CakeWriting { get; set; }
+
+        /* The constructor sets up the object's state by setting the properties so that it can calculate
+         * the cost later.*/
+        public BirthdayParty(int numberOfPeople, bool fanceDecorations, string cakeWriting)
+        {
+            this.NumberOfPeople = numberOfPeople;
+            this.FancyDecorations = fanceDecorations;
+            this.CakeWriting = cakeWriting;
+        }
     }
 }
