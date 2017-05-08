05359db24195ac82194567c5fa7ecf91ffd433fa
Jose Castillo <Jose Castillo>
2017-05-07 22:00:30 -0500

    Added a couple of tabs to the windows form so that this application can have a Dinner Party tab and a Birthday Party tab to perform their corresponding operations and calculations

--- EventPlanningCostCalculator/Form1.Designer.cs
+++ EventPlanningCostCalculator/Form1.Designer.cs
@@ -39,21 +39,27 @@
             this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.label2 = new System.Windows.Forms.Label();
             this.costLabel = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 25);
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.Size = new System.Drawing.Size(92, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Number of People";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(28, 48);
+            this.numericUpDown1.Location = new System.Drawing.Point(19, 31);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             1000,
             0,
@@ -65,7 +71,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(136, 26);
+            this.numericUpDown1.Size = new System.Drawing.Size(91, 20);
             this.numericUpDown1.TabIndex = 1;
             this.numericUpDown1.Value = new decimal(new int[] {
             5,
@@ -79,9 +85,10 @@
             this.fancyBox.AutoSize = true;
             this.fancyBox.Checked = true;
             this.fancyBox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.fancyBox.Location = new System.Drawing.Point(28, 107);
+            this.fancyBox.Location = new System.Drawing.Point(19, 70);
+            this.fancyBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fancyBox.Name = "fancyBox";
-            this.fancyBox.Size = new System.Drawing.Size(168, 24);
+            this.fancyBox.Size = new System.Drawing.Size(115, 17);
             this.fancyBox.TabIndex = 2;
             this.fancyBox.Text = "Fancy Decorations";
             this.fancyBox.UseVisualStyleBackColor = true;
@@ -90,9 +97,10 @@
             // healthyBox
             // 
             this.healthyBox.AutoSize = true;
-            this.healthyBox.Location = new System.Drawing.Point(28, 147);
+            this.healthyBox.Location = new System.Drawing.Point(19, 96);
+            this.healthyBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.healthyBox.Name = "healthyBox";
-            this.healthyBox.Size = new System.Drawing.Size(323, 24);
+            this.healthyBox.Size = new System.Drawing.Size(219, 17);
             this.healthyBox.TabIndex = 3;
             this.healthyBox.Text = "Healthy Option (No alcohol, 5% discount)";
             this.healthyBox.UseVisualStyleBackColor = true;
@@ -112,38 +120,72 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 212);
+            this.label2.Location = new System.Drawing.Point(16, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.Size = new System.Drawing.Size(32, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Cost";
             // 
             // costLabel
             // 
             this.costLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.costLabel.Location = new System.Drawing.Point(87, 212);
+            this.costLabel.Location = new System.Drawing.Point(58, 138);
+            this.costLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.costLabel.Name = "costLabel";
-            this.costLabel.Size = new System.Drawing.Size(108, 24);
+            this.costLabel.Size = new System.Drawing.Size(72, 16);
             this.costLabel.TabIndex = 5;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-1, -3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(247, 174);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(239, 148);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "DinnerParty";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(239, 148);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Birthday Party";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 261);
+            this.ClientSize = new System.Drawing.Size(239, 170);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.costLabel);
             this.Controls.Add(this.label2);
-
             this.Controls.Add(this.healthyBox);
             this.Controls.Add(this.fancyBox);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Party Planner";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -161,6 +203,9 @@
         private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
